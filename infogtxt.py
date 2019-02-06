b932f19fd83b7dfb1220805742ab7cd12334223b
RW Martin <rw-martin@users.noreply.github.com>
2019-02-06 15:45:08 +0800

    First pass at cleaning up the file

--- infogtxt.py
+++ infogtxt.py
@@ -1,28 +1,34 @@
 from PIL import Image, ImageFont, ImageDraw
+import random
 
 def generateInfoGraphic(info,typeface,fontSize,R,G,B):
 
     # coords for where to place text
     xpos = [421, 282, 90, 478, 684]
     ypos = [12, 172, 172, 172, 172]
+    months=["January","February","March","April","May","June","July","August",
+            "September","October","November","December"]
 
-    info[0]=info[0]+", here are your numbers for November..."
     img = Image.open("mytemplate.png")
+    index = 0
+    filename = ""
 
-    index=0
     for detail in info:
-        offset = (len(detail)/2)*14
-        xcord = xpos[index] - offset
+
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype("Microsoft Sans Serif.ttf", fontSize)
-        if (index > 0):
+        if index > 0:
             # add commas to numbers
-            detail="{:,}".format(int(detail))
+            detail = "{:,}".format(int(detail))
         else:
-            filename=detail.split(",")[0]
+            detail = detail + ", here are your numbers for " + months[random.randint(0, 11)] + "..."
+            filename = detail.split(",")[0]
+
+        offset = (len(detail) / 2) * 14
+        xcord = xpos[index] - offset
         draw.multiline_text((xcord, ypos[index]), detail, (R, G, B), font=font)
-        index=index+1
-    #str(uuid.uuid4())
+        index = index + 1
+
     img.save('sample-infographic-' + filename + '.png')
     index = 0
 
@@ -37,5 +43,5 @@ myData = readMyFile('mydata.csv')
 
 for line in myData:
     info = line.split(",")
-    info[4]=info[4].strip("\n")
+    info[4] = info[4].strip("\n")
     generateInfoGraphic(info,"Arial.ttf",32,55,145,192)
